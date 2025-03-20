757f5d2193d5e20da5bcdc7da3498158b239681d
philip-s-kuti <v3ta6@id415m46.cs.unb.ca>
2025-03-20 15:20:03 -0300

    Added renameFile and changeContents methods

--- File.h
+++ File.h
@@ -1,12 +1,27 @@
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct {
+typedef struct File File;
+
+struct File {
 	char *name;
 	char *contents;
 	int size;
 	char *dateModified;
 	char *dateCreated;
-	
-} File;
+};
+
+void renameFile(File *file, char *nameIn) {
+	realloc(file->name, sizeof(nameIn));
+	file->name = strdup(nameIn);
+}
+
+void changeContents(File *file, char *contentsIn, char *dateIn) {
+	realloc(file->contents, sizeof(contentsIn));
+	file->contents = strdup(contentsIn);
+	realloc(file->dateModified, sizeof(dateIn));
+	file->dateModified = strdup(dateIn);
+}
+
 
 
