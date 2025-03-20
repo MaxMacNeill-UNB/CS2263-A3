d9f7de5e880c3b25cd9c84f7dca87eaeb3bfc668
Max MacNeill <d3n2c@unb.ca>
2025-03-20 14:57:55 -0300

    Implemented initial directory

---
+++ Directory.h
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+typedef struct Directory Directory;
+
+struct Directory {
+    Directory* parent;
+    Directory** subdirectories;
+    int subdirectoryCount;
+    int maxSubdirectoryCount;
+    File** files;
+    int fileCount;
+    int maxFileCount;
+};
+
+Directory *newDir(Directory *p) {
+    Directory *ndir = malloc(sizeof(Directory));
+    ndir->parent = p;
+    ndir->subdirectories = malloc(sizeof(*Directory) * 5);
+    ndir->subdirectoryCount = 0;
+    ndir->maxSubdirectoryCount = 5;
+    ndir->files = malloc(sizeof(*File) * 5);
+    ndir->fileCount = 0;
+    ndir->maxFileCount = 5;
+}
